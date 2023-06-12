c60831fd21c5c7bcfc69fbf2b63937adce7f5afa
Dimoxie <durastiven@gmail.com>
2023-06-12 01:37:29 -0500

    sexy

---
+++ cred.py
@@ -0,0 +1,22 @@
+#Twilio Details
+
+account_sid = 'Enter Twilio Acc Sid'
+auth_token = 'Enter Twilio Acc Auth Token'
+twilionumber = 'Enter Twilio Acc Number'
+twiliosmsnumber = 'Enter Twilio Acc Number'
+
+#FC Bot
+API_TOKEN = "Enter Your Bot Token Here"
+
+#Host URL
+callurl = 'Enter Your Heroku URL'
+twiliosmsurl = 'Enter Your Heroku URL/sms'
+
+
+
+
+
+
+
+
+
