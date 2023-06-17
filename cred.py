716c930c2062b647345ee6fe9261cd1de062bd01
Payton1611 <108036817+Payton1611@users.noreply.github.com>
2023-06-16 21:41:02 -0500

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,12 +1,12 @@
 #Twilio Details
 
-account_sid = 'Enter Twilio Acc Sid'
-auth_token = 'Enter Twilio Acc Auth Token'
-twilionumber = 'Enter Twilio Acc Number'
-twiliosmsnumber = 'Enter Twilio Acc Number'
+account_sid = 'AC63c19353a1c0c7e276635176b1195480'
+auth_token = '6a4d3c4025b5a4e487e2ebfc7b164f93'
+twilionumber = '+13613663566'
+twiliosmsnumber = '+13613663566'
 
 #FC Bot
-API_TOKEN = "Enter Your Bot Token Here"
+API_TOKEN = "6019235548:AAF3jKhhNAvumv8JvB5I1nFAXz255654ewo"
 
 #Host URL
 callurl = 'Enter Your Heroku URL'
